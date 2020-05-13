956813818d939405607033df191e79df5a78eded
yamahare <jidai.norio@gmail.com>
2020-05-13 11:02:46 +0900

    [add]title消した

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ function Layout(props){
     return (
         <div>
             <Head>
-                <title>{props.title}</title>
                 <meta charSet='utf-8' />
                 <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             </Head>
